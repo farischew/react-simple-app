590391752fa84d6c1a9950a624f6703c28d6bd0d
Faris Chew <farischew@gmail.com>
2023-05-02 23:02:57 +0800

    Adds dynamic ErrorModal

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,7 +3,15 @@ import React from "react";
 import styles from "./Button.module.css";
 
 const Button = (props) => {
-  return <button className={styles.button}>{props.children}</button>;
+  return (
+    <button
+      className={styles.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
