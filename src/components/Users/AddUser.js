268683c0efb6a23ef4fa5fd64aed064ab7e43dd5
Faris Chew <farischew@gmail.com>
2023-05-02 21:42:31 +0800

    Adds form in AddUser

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddUser = (props) => {
+  <form>
+    <label htmlFor="username">Username</label>
+    <input id="username" type="text"></input>
+    <label htmlFor="age">Age</label>
+    <input id="age" type="number"></input>
+  </form>;
+};
+
+export default AddUser;
