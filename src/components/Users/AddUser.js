596b5c6d94e97330c87c29a3eeb603cc61fa2f0f
Faris Chew <farischew@gmail.com>
2023-05-02 21:48:26 +0800

    Adds form in App.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -5,13 +5,15 @@ const AddUser = (props) => {
     event.preventDefault();
   };
 
-  <form onSubmit={addUserHandler}>
-    <label htmlFor="username">Username</label>
-    <input id="username" type="text"></input>
-    <label htmlFor="age">Age</label>
-    <input id="age" type="number"></input>
-    <button type="submit">Add User</button>
-  </form>;
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text"></input>
+      <label htmlFor="age">Age</label>
+      <input id="age" type="number"></input>
+      <button type="submit">Add User</button>
+    </form>
+  );
 };
 
 export default AddUser;
