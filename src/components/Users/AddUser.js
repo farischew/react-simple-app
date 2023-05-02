60dbad6860a00802c88df367014268a2d34b1902
Faris Chew <farischew@gmail.com>
2023-05-02 22:04:45 +0800

    Styles form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
