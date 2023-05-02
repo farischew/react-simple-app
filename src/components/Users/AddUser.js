29652f35864eceb74b3f044e0a10de0ad913be3a
Faris Chew <farischew@gmail.com>
2023-05-02 21:43:39 +0800

    Adds form in AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const AddUser = (props) => {
-  <form>
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  <form onSubmit={addUserHandler}>
     <label htmlFor="username">Username</label>
     <input id="username" type="text"></input>
     <label htmlFor="age">Age</label>
     <input id="age" type="number"></input>
+    <button type="submit">Add User</button>
   </form>;
 };
 
